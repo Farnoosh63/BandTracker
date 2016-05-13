aa6447d520c7527bca5f71a373ef4e8b19925284
farnoosh <farnoosh@pdx.edu>
2016-05-13 08:33:08 -0700

    add db java file

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.List;
+
+import java.util.ArrayList;
+
+public class App {
+  public static void main (String[] args){
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+
+
+
+
+  }
