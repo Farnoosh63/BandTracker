9d17d384638ef4b03487ba55abd9ac18bc19f9ea
farnoosh <farnoosh@pdx.edu>
2016-05-13 08:34:55 -0700

    add band.java

---
+++ src/main/java/Band.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import org.sql2o.*;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+
+public class Band {
+  private int id;
+  private String name;
+
+  public Band(String name) {
+    this.name = name;
+  }
+}
