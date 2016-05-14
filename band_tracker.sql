d9a2c2bf46683114304603366e5cf23ab062d837
farnoosh <farnoosh@pdx.edu>
2016-05-13 18:12:06 -0700

    add sql file

---
+++ band_tracker.sql
@@ -0,0 +1,235 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: bands; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
+--
+
+CREATE TABLE bands (
+    id integer NOT NULL,
+    name character varying
+);
+
+
+ALTER TABLE bands OWNER TO "Guest";
+
+--
+-- Name: bands_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE bands_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE bands_id_seq OWNER TO "Guest";
+
+--
+-- Name: bands_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE bands_id_seq OWNED BY bands.id;
+
+
+--
+-- Name: bands_venues; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
+--
+
+CREATE TABLE bands_venues (
+    id integer NOT NULL,
+    bandid integer,
+    venueid integer
+);
+
+
+ALTER TABLE bands_venues OWNER TO "Guest";
+
+--
+-- Name: bands_venues_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE bands_venues_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE bands_venues_id_seq OWNER TO "Guest";
+
+--
+-- Name: bands_venues_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE bands_venues_id_seq OWNED BY bands_venues.id;
+
+
+--
+-- Name: venues; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
+--
+
+CREATE TABLE venues (
+    id integer NOT NULL,
+    location character varying
+);
+
+
+ALTER TABLE venues OWNER TO "Guest";
+
+--
+-- Name: venues_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE venues_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE venues_id_seq OWNER TO "Guest";
+
+--
+-- Name: venues_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE venues_id_seq OWNED BY venues.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY bands ALTER COLUMN id SET DEFAULT nextval('bands_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY bands_venues ALTER COLUMN id SET DEFAULT nextval('bands_venues_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY venues ALTER COLUMN id SET DEFAULT nextval('venues_id_seq'::regclass);
+
+
+--
+-- Data for Name: bands; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY bands (id, name) FROM stdin;
+\.
+
+
+--
+-- Name: bands_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('bands_id_seq', 8, true);
+
+
+--
+-- Data for Name: bands_venues; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY bands_venues (id, bandid, venueid) FROM stdin;
+\.
+
+
+--
+-- Name: bands_venues_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('bands_venues_id_seq', 21, true);
+
+
+--
+-- Data for Name: venues; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY venues (id, location) FROM stdin;
+\.
+
+
+--
+-- Name: venues_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('venues_id_seq', 10, true);
+
+
+--
+-- Name: bands_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
+--
+
+ALTER TABLE ONLY bands
+    ADD CONSTRAINT bands_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: bands_venues_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
+--
+
+ALTER TABLE ONLY bands_venues
+    ADD CONSTRAINT bands_venues_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: venues_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
+--
+
+ALTER TABLE ONLY venues
+    ADD CONSTRAINT venues_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: epicodus
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM epicodus;
+GRANT ALL ON SCHEMA public TO epicodus;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
